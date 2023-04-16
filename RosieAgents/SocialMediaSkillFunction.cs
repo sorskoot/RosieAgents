4301019e55318f40087a4d1e4924ea20b92f329e
Sorskoot <mental_c@hotmail.com>
2023-04-16 22:08:35 +0200

    Added start of social Media Skills

---
+++ RosieAgents/SocialMediaSkillFunction.cs
@@ -0,0 +1,52 @@
+using System.Collections.Specialized;
+using System.Net;
+using System.Web;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel.Orchestration;
+
+namespace RosieAgents
+{
+    public class SocialMediaSkillFunction:SkillFunctionBase
+    {
+        public SocialMediaSkillFunction(ILoggerFactory loggerFactory):base(loggerFactory) {}
+
+        [Function("SocialMediaSkill")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+        {
+            InitKernel();
+
+            NameValueCollection queryDictionary = HttpUtility.ParseQueryString(req.Url.Query);
+
+            if (!queryDictionary.HasKeys())
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            string requestedSkill = queryDictionary["skill"] ?? string.Empty;
+            string requestedInput = queryDictionary["input"] ?? string.Empty;
+
+            if (string.IsNullOrWhiteSpace(requestedSkill) || string.IsNullOrWhiteSpace(requestedInput))
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            IDictionary<string, ISKFunction> skill = GetSemanticsSkill("SocialMediaSkill");
+
+            if (!skill.ContainsKey(requestedSkill))
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            SKContext result = await this.Kernel.RunAsync(requestedInput, skill[requestedSkill]);
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            await response.WriteStringAsync(result.Result);
+
+            return response;
+        }
+    }
+}

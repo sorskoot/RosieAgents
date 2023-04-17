04778a4fa1472f351aff37e5fd5a25d31945aa1a
Sorskoot <mental_c@hotmail.com>
2023-04-17 19:56:04 +0200

    added function to get youtube transcript

---
+++ RosieAgents/GetYoutubeTranscript.cs
@@ -0,0 +1,56 @@
+using System.Collections.Specialized;
+using System.Net;
+using System.Web;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using YoutubeTranscriptApi;
+
+namespace RosieAgents
+{
+    public class GetYoutubeTranscript
+    {
+        private readonly ILogger logger;
+
+        public GetYoutubeTranscript(ILoggerFactory loggerFactory)
+        {
+            logger = loggerFactory.CreateLogger<GetYoutubeTranscript>();
+        }
+
+        [Function("GetYoutubeTranscript")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+        {
+
+            NameValueCollection queryDictionary = HttpUtility.ParseQueryString(req.Url.Query);
+
+            if (!queryDictionary.HasKeys())
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            string videoId = queryDictionary["video_id"] ?? string.Empty;
+
+            if (string.IsNullOrWhiteSpace(videoId))
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            string result;
+            using (var youTubeTranscriptApi = new YouTubeTranscriptApi())
+            {
+                IEnumerable<TranscriptItem> transcriptItems = youTubeTranscriptApi.GetTranscript(videoId);
+
+                result = string.Join('\n',
+                    from transcriptItem in transcriptItems
+                    select transcriptItem.Text);
+            }
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString(result);
+
+            return response;
+        }
+    }
+}
---
+++ RosieAgents/apis/YoutubeTranscriptApi/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace YoutubeTranscriptApi
+{
+    internal static class Settings
+    {
+        public const string WATCH_URL = "https://www.youtube.com/watch?v={0}";
+    }
+}
